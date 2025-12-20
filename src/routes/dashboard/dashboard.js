9378ad52e19b1ba141f7417644eff3e6a4474fb9
aponchikaj <lazzexa@gmail.com>
2025-12-21 00:28:42 +0400

    added some stuff on dashboard

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import whoami from '../../middlewares/whoami.js';
 import USERS from '../../models/user.model.js';
-
+import BUILDINGS from '../../models/building.model.js'
 
 router.get('/api/dashboard',whoami,async(req,res)=>{
 
@@ -14,10 +14,24 @@ router.get('/api/dashboard',whoami,async(req,res)=>{
             return res.send({Success:false,Message:"Something went wrong."})
         }
 
+        const result = await BUILDINGS.aggregate([
+        { $match: { owner: USER._id } },
+        { $unwind: "$maps" },
+        {
+            $group: {
+            _id: null,
+            totalScans: { $sum: "$maps.scanned" }
+            }
+        }
+        ])
+
+        const totalScans = result[0]?.totalScans || 0
+
         return res.send({Success:true,Message:{
             MyBuildings:USER.Buildings.length,
             scanned:USER.scanned.length,
-            lastScanned: USER.scanned[USER.scanned.length - 1] || null
+            lastScanned: USER.scanned[USER.scanned.length - 1] || null,
+            myBuildingsScanned: totalScans
         }})
     }catch{
         return res.send({Success:false,Message:'Server error.'})
