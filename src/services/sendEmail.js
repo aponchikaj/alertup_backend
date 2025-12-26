84815bf440eb5c1d3d7ce4c5076ca2b40f3c6cbd
aponchikaj <lazzexa@gmail.com>
2025-12-26 22:08:41 +0400

    fixed bug

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -4,23 +4,24 @@ import 'dotenv/config';
 const transport = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
   port: 587,
+  secure: false, // TLS is used automatically with STARTTLS
   auth: {
-    user: "apikey", // this must literally be "apikey"
+    user: "apikey", // must literally be "apikey"
     pass: process.env.SENDGRID_API_KEY
-  }
+  },
+  tls: {
+    rejectUnauthorized: false // sometimes required on cloud hosts
+  },
+  connectionTimeout: 10000 // 10 seconds
 });
 
-const sendMail = async (to, subject, text, html) => {
-  if (!to || !subject || (!text && !html)) {
-    return false;
-  }
+const sendMail = async (to, subject, text) => {
 
   await transport.sendMail({
     from: "lazaremirziashvili@alertup.world", // use your verified sender
     to,
     subject,
     text,
-    html
   });
 
   return true;
