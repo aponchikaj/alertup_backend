7269981c9376acd9cabfb0eb08653e8b419401eb
aponchikaj <lazzexa@gmail.com>
2025-12-26 19:20:25 +0400

    sokfw

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cparser())
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://alertup.vercel.app',
   credentials: true
 }));
 app.use(bparser.json())
